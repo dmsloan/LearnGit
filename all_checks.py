702455fecb6adcb776fe0a74acf9ec6c23fffef2
dmsloan from Dell Latitude <dsloan@ibeinc.com>
2021-09-16 08:23:30 -0700

    Add a message when all ok

--- all_checks.py
+++ all_checks.py
@@ -2,11 +2,16 @@
 #/usr/bin/env python3
 
 # ! C:/Program Files/Python38 python3
+import os
+import sys
 
 def check_reboot():
     """Returns true if the computer has a pending reboot."""
 
 def main()
-    pass
+    if check_reboot():
+        print("Pending reboot.")
+        sys.exit(1)
+    print("Everything ok.")
 
 main()
